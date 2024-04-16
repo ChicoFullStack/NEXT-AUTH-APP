ca83932f2304a14931b5023a804d8df6dee50515
ChicoFullStack <140274298+ChicoFullStack@users.noreply.github.com>
2024-04-16 18:08:12 -0300

    menu e layout

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,5 +1,14 @@
+"use client"
+import React from "react";
+import Layout from "@/components/layout";
 import UserInfo from "@/components/UserInfo";
 
 export default function Dashboard() {
-  return <UserInfo />;
+  return (
+    <>
+      <Layout>
+        <UserInfo />
+      </Layout>
+    </>
+  );
 }
