090bf8cd761ae8c6620d498d9db896b0661a0fdc
ChicoFullStack <140274298+ChicoFullStack@users.noreply.github.com>
2024-04-16 14:55:06 -0300

    img background

--- components/Loginform.jsx
+++ components/Loginform.jsx
@@ -36,7 +36,7 @@ export default function LoginForm() {
   };
 
   return (
-    <div className="grid place-items-center h-screen">
+    <div className="grid place-items-center h-screen bg-yellow-400">
       <div className="shadow-lg p-5 rounded-lg border-t-4 border-blue-400">
         {/* <h1 className="text-xl font-bold my-4">Login</h1> */}
         <div className="grid place-items-center py-2">
