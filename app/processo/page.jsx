286338007c3452dd703c2ed5f3054eb8974f7466
ChicoFullStack <140274298+ChicoFullStack@users.noreply.github.com>
2024-04-17 23:50:04 -0300

    dash, processo, cadastro 19

--- app/processo/page.jsx
+++ app/processo/page.jsx
@@ -1,50 +1,23 @@
-"use client"
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Layout from "../../components/layout";
-import Card from "../../components/Card";
-import CircularProgress from "@mui/material/CircularProgress";
-import styles from "./Projetos.module.css";
+// import Card from "../../components/Card";
+// import CircularProgress from "@mui/material/CircularProgress";
+// import styles from "./Projetos.module.css";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "../api/auth/[...nextauth]/route";
+import ProcessAnalytics from "@/components/ProcessAnalityc/Process";
 
-export default function Analytics() {
-
-  const session = getServerSession(authOptions);
+export default async function Analytics() {
+  const session = await getServerSession(authOptions);
 
   if (!session) {
     redirect("/dashboard");
   }
-  
-  const [repositories, setRepositories] = useState([]);
-
-  useEffect(() => {
-    const buscarRepositorios = async () => {
-      const response = await fetch(
-        "https://api-opcsmart-a48986e53ec1.herokuapp.com/api/list-one"
-      );
-      const data = await response.json();
-      setRepositories(data);
-    };
-    buscarRepositorios();
-  }, [repositories]);
 
   return (
     <Layout>
-      <div className={styles.container_o}>
-        <section className={styles.projetos}>
-          {/* <h2>Projetos</h2> */}
-          {repositories.length > 0 ? (
-            <section className={styles.lista}>
-              <Card />
-            </section>
-          ) : (
-            
-              <CircularProgress size={80}/>
-            
-          )}
-        </section>
-      </div>
+      <ProcessAnalytics />
     </Layout>
   );
 }
