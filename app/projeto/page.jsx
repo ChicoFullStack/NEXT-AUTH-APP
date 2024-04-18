9d2e11fe74cb1501fcb516a514f6e17ef6bec018
ChicoFullStack <140274298+ChicoFullStack@users.noreply.github.com>
2024-04-17 22:48:59 -0300

    dash, processo, cadastro 11

--- app/projeto/page.jsx
+++ app/projeto/page.jsx
@@ -16,7 +16,7 @@ export default async function Projeto() {
   const session = await getServerSession(authOptions);
 
   if (session) {
-    redirect("/projeto");
+    redirect("/dashboard");
   }
   return (
     <>
